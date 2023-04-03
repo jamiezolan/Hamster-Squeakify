e108c5fbb11f9440fa3a5814caf16cc897fab5bc
Jamie Zolan <jamiezolan@live.com>
2023-04-03 13:41:13 -0500

    manifest v3 changes

--- popup.js
+++ popup.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const squeakifyButton = document.getElementById('squeakify');
   
-    squeakifyButton.addEventListener('click', () => {
+    squeakifyButton.addEventListener('click', async () => {
       // Inject the content script into the active tab
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.executeScript(tabs[0].id, { file: 'content.js' }, () => {
-          // Send a message to the content script to squeakify the page
-          chrome.tabs.sendMessage(tabs[0].id, { action: 'squeakify' });
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
-          // Close the popup after the button is clicked
-          window.close();
-        });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['content.js']
+      }, () => {
+        // Send a message to the content script to squeakify the page
+        chrome.tabs.sendMessage(tab.id, { action: 'squeakify' });
+  
+        // Close the popup after the button is clicked
+        window.close();
       });
     });
   });
