351db750fba963081cc708e57598f5063b890eb2
Jamie Zolan <jamiezolan@live.com>
2023-04-03 13:29:21 -0500

    injection of content script

--- popup.js
+++ popup.js
@@ -2,13 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const squeakifyButton = document.getElementById('squeakify');
   
     squeakifyButton.addEventListener('click', () => {
-      // Send a message to the content script to squeakify the page
+      // Inject the content script into the active tab
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: 'squeakify' });
-      });
+        chrome.tabs.executeScript(tabs[0].id, { file: 'content.js' }, () => {
+          // Send a message to the content script to squeakify the page
+          chrome.tabs.sendMessage(tabs[0].id, { action: 'squeakify' });
   
-      // Close the popup after the button is clicked
-      window.close();
+          // Close the popup after the button is clicked
+          window.close();
+        });
+      });
     });
   });
-  
